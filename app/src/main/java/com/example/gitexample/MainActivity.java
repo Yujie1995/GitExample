056e023d9268b867fb46f027d95a78cb29b1e504
Yujie <b0972911675@gmail.com>
2021-08-15 23:52:54 +0800

    Fix in Main2 from aseijuy1995

--- app/src/main/java/com/example/gitexample/MainActivity.java
+++ app/src/main/java/com/example/gitexample/MainActivity.java
@@ -183,5 +183,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO: 2021/8/15
-    // TODO: 2021/8/15 Fix in main2 
+    // TODO: 2021/8/15 Fix in main2
+    // TODO: 2021/8/15 Fix in main2-2
 }
